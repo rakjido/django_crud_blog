34da5e7858ec3ddbf645de5ec14346cedf426456
titan7 <titan7@titan9ui-MacBook-Pro.local>
2018-11-19 18:45:01 +0900

    v0.2 * 댓글기능 추가

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # django crud blog
 
+## v0.2
+* 댓글기능 추가
+
 ## v0.1
 * basic CRUD blog
 * django.contrib.auth.decorators를 이용한 login_required 추가
--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
